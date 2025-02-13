4241457abd9db13ddabe981ee54ad6cec387814e
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-02-13 13:24:03 -0300

    finalizando script

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Arquivo Javascript carregado com sucesso!')
+document.addEventListener("DOMContentLoaded", () => {
+	const courseForm = document.getElementById("course-name");
+	const courseList = document.getElementById("course-list");
+
+
+	courseForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+
+		const name = document.getElementById("course-name").value;
+		const description = document.getElementById("course-description").value;
+		const duration = document.getElementById("course-duration").value;
+
+		if (name && description && duration) {
+			const listItem = document.createElement("li");
+			listItem.innerHTML = `<strong>${name}</strong> - ${description} (${duration} hours)`;
+			courseList.appendChild(listItem);
+
+			courseForm.reset();
+		} else {
+			alert("Please fill out all fields!");
+		}
+	});
+});
